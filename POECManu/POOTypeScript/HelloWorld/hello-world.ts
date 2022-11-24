328c183a934d8159adbfb137ac5e51dd10e4feb4
ManuAngot <manu.angot@hotmail.fr>
2022-11-24 16:51:37 +0100

    ajours cours Git

---
+++ POECManu/POOTypeScript/HelloWorld/hello-world.ts
@@ -0,0 +1,17 @@
+console.log("Hello World !!! ^^");
+
+const prenom : string = "bitos";
+// const variable ! <type> = <valeur>;
+// let variable ! <type> = <valeur>;  lorsque la variable doit changer
+// nombre : type number
+
+//concaténation
+console.log("coucou " + prenom + " lol");
+console.log(`Coucou ${prenom} lol`);
+
+function helloWorld() : void{
+   // console.log("Hello World");
+   //return ("hello World ");
+}
+console.log("Hello World");
+//helloWorld();
---
+++ POECManu/POOTypeScript/index.js
@@ -0,0 +1,34 @@
+/* 
+*****************************************************
+**************** POO Type-Script ********************
+*****************************************************
+ouverture powerShell en mode admin : click droit pour installer sans problème
+
+Type script : installation
+npm install -g typescript
+
+si problème : dans invite de commande (administrateur) :
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+Exécutzer durectement le type scriot:
+npm i -g ts-node
+
+On pourra démarrer projets avec :
+ts-node <nom-fichier>
+
+On pourra démarrer projets avec version JS:
+tsc .\nomdeFichier.ts
+node .\nomdeFichier.ts
+Mais dédouble fichier en js
+
+
+type unknow à revoir pour expliciter un type à un temps T
+cf cours
+Name (param): string =``
+
+Fonctions
+const mafonction = () =>{
+
+}
+*/
+
