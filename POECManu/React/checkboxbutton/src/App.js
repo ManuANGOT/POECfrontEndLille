da5ad7203e295645afdfc72ca5dac1ce47bf8df5
ManuANGOT <manu.angot@hotmail.fr>
2022-12-08 09:18:05 +0100

    ajout des cours React et modifs

--- POECManu/React/checkboxbutton/src/App.js
+++ POECManu/React/checkboxbutton/src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
+import Bouton from "./components/Bouton";
 import Checkbox from "./components/Checkbox";
+import ContactList from "./components/ContactList";
+import Count from "./components/Count";
+import { Route, Routes} from 'react-router-dom';
+import Home from "./components/pages/Home";
+import {Link} from "react-router-dom";
 
 
 function App() {
-  return (
-    
-    <>
-    <h1>Exo checkbox/button</h1>
-    <Checkbox />
-    
-    </>
-  );
+
+    return (
+      <>
+      <ul>
+        <Link to={"/"}>Accueil</Link>
+        <Link to={"/contact"}>Contacts</Link>
+        <Link to={"/count"}>Count</Link>
+
+      </ul>
+
+      <Routes>
+      <Route path="/" element ={<Home />} />
+       <Route path="/contact" element ={<ContactList />}/>
+        <Route path="/count" element ={<Count />}/>
+    </Routes>
+
+      
+      </>
+
+    )
+
 }
 
 export default App;
+
