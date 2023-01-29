bda297506e1ab1cc4b57df7f58ad5bbd09697dda
Emmanuel ANGOT <manu.angot@hotmail.fr>
2023-01-29 15:12:07 +0100

    Ajout bouton et fonctioallité, emmanyual Angot 29 janvier

--- POECManu/React/checkboxbutton/src/components/Bouton.jsx
+++ POECManu/React/checkboxbutton/src/components/Bouton.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 export default function Bouton () {
     const [on, setOn] = useState(true);
     return (
+        setOn(!on)
+        return (
+          <input type = "checked" checked ={on} on/>
         
     )
   
